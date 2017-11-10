cfc860b5668ee5708e7031a8984f26b72d8897aa
Christopher Lee <hanernlee@gmail.com>
2017-11-11 01:34:12 +1100

    Adding Employees and tuples

---
+++ IntermediateTraining/IntermediateTraining/Companies/Employees/EmployeesController.swift
@@ -0,0 +1,88 @@
+//
+//  EmployeesController.swift
+//  IntermediateTraining
+//
+//  Created by Christopher Lee on 9/11/17.
+//  Copyright © 2017 Christopher Lee. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class EmployeesController: UITableViewController, CreateEmployeeControllerDelegate {
+    
+    var company: Company?
+    
+    var employees = [Employee]()
+    
+    let cellID = "employeeCell"
+    
+    func didAddEmployee(employee: Employee) {
+        employees.append(employee)
+        
+        let newIndexPath = IndexPath(row: employees.count - 1, section: 0)
+        tableView.insertRows(at: [newIndexPath], with: .automatic)
+        
+//        tableView.reloadData()
+    }
+    
+    // MARK: - View Lifecycle
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationItem.title = company?.name
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.backgroundColor = .darkBlue
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
+        
+        setupPlusButtonInNav(selector: #selector(handleAdd))
+        
+        fetchEmployees()
+    }
+    
+    private func fetchEmployees() {
+        let context = CoreDataManager.shared.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<Employee>(entityName: "Employee")
+        
+        do {
+            let employees = try context.fetch(request)
+            
+            self.employees = employees
+        } catch let error {
+            print("Failed to fetch employees", error)
+        }
+    }
+    
+    @objc private func handleAdd() {
+        print("Trying to add employee")
+        
+        let createEmployeeController = CreateEmployeeController()
+        createEmployeeController.delegate = self
+        let navController = UINavigationController(rootViewController: createEmployeeController)
+        
+        present(navController, animated: true, completion: nil)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return employees.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        
+        let employee = employees[indexPath.row]
+        
+        cell.textLabel?.text = employee.name
+        cell.textLabel?.textColor = .white
+        cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 15)
+        cell.backgroundColor = .tealColor
+        
+        return cell
+    }
+}
--- IntermediateTraining/IntermediateTraining/CoreDataManager.swift
+++ IntermediateTraining/IntermediateTraining/CoreDataManager.swift
@@ -36,19 +36,19 @@ struct CoreDataManager {
         }
     }
     
-    func createEmployee(employeeName: String) -> Error? {
+    func createEmployee(employeeName: String) -> (Employee?, Error?) {
         let context = persistentContainer.viewContext
         
-        let employee = NSEntityDescription.insertNewObject(forEntityName: "Employee", into: context)
+        let employee = NSEntityDescription.insertNewObject(forEntityName: "Employee", into: context) as! Employee
         
         employee.setValue(employeeName, forKey: "name")
         
         do {
             try context.save()
-            return nil
+            return (employee, nil)
         } catch let saveError {
             print("Failed to save data:", saveError)
-            return saveError
+            return (nil, saveError)
         }
     }
 }
